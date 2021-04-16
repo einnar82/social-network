b48f4841b418b0ae00fc49c8e0884a2fc32cfca7
Rannie Ollit <rannie.ollit@petnet.com.ph>
2021-04-17 07:29:29 +0800

    Enhanced test

--- tests/Feature/CommentTest.php
+++ tests/Feature/CommentTest.php
@@ -39,4 +39,43 @@ class CommentTest extends TestCase
 
         $response->assertStatus(422);
     }
+
+    public function testCreateCommentSuccessWithParent()
+    {
+        $createdResponse = $this->postJson('/api/comments', [
+            'comment_text' => 'Wew',
+            'name' => 'rollit',
+        ]);
+
+        $created = json_decode($createdResponse->getContent());
+
+        $response = $this->postJson('/api/comments', [
+            'comment_text' => 'Whoppy',
+            'parent_id' => "$created->id",
+            'name' => 'das',
+        ]);
+
+        $response->assertStatus(201);
+    }
+
+    public function testIfCanFetchChildrenComments()
+    {
+        $createdResponse = $this->postJson('/api/comments', [
+            'comment_text' => 'Wew',
+            'name' => 'rollit',
+        ]);
+
+        $created = json_decode($createdResponse->getContent());
+
+        $response = $this->getJson("/api/comments/$created->id");
+
+        $response->assertStatus(200);
+    }
+
+    public function testIfChildrenCommentsNotFound()
+    {
+        $response = $this->getJson('/api/comments/100000');
+
+        $response->assertStatus(404);
+    }
 }
