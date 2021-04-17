19e6693546e7cca0c00e64ffcb07a59f48f6d830
Rannie Ollit <rannie.ollit@petnet.com.ph>
2021-04-17 13:05:42 +0800

    Added grandchild

--- app/Comment.php
+++ app/Comment.php
@@ -9,10 +9,20 @@ class Comment extends Model
     protected $guarded = ['id'];
 
     public const MAX_LEVEL = 3;
+    public const PARENT = 1;
+    public const CHILDREN = 2;
+    public const GRANDCHILD = 3;
 
     public function children()
     {
-        return $this->hasMany(self::class, 'parent_id')->whereNotNull('parent_id')->latest();
+        return $this->hasMany(self::class, 'parent_id')
+            ->whereNotNull('parent_id')->where('level', self::CHILDREN)->latest();
+    }
+
+    public function grand_children()
+    {
+        return $this->hasMany(self::class, 'parent_id')
+            ->whereNotNull('parent_id')->where('level', self::GRANDCHILD)->latest();
     }
 
 }
